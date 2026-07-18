db017580f6027daa074ab72dd670ee7b2609c800
ngodongnguyen <nguyen.ngo.1@smartpayvn.com>
2026-07-18 12:47:30 +0700

    add metric to medical

--- test.sh
+++ test.sh
@@ -3,146 +3,146 @@ device=0
 
 NOTIFY_SCRIPT=./scripts/notify.py
 
-LOG=${save_dir}"res.log"
-echo ${LOG}
-depth=(9)
-n_ctx=(12)
-t_n_ctx=(4)
-for i in "${!depth[@]}";do
-    for j in "${!n_ctx[@]}";do
-    ## train on the VisA dataset
-        base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale
-        save_dir=./checkpoints/${base_dir}/
-        CUDA_VISIBLE_DEVICES=${device} python test.py --dataset mvtec \
-        --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/mvtecdataset --save_path ./results/${base_dir}/mvtec \
-        --checkpoint_path ${save_dir}epoch_15.pth \
-        --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
-        --print_top_k 10
-    wait
-    done
-done
-python ${NOTIFY_SCRIPT} --mode test --dataset mvtec --log_path ./results/${base_dir}/mvtec/log.txt --exp_name ${base_dir}
+# LOG=${save_dir}"res.log"
+# echo ${LOG}
+# depth=(9)
+# n_ctx=(12)
+# t_n_ctx=(4)
+# for i in "${!depth[@]}";do
+#     for j in "${!n_ctx[@]}";do
+#     ## train on the VisA dataset
+#         base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale
+#         save_dir=./checkpoints/${base_dir}/
+#         CUDA_VISIBLE_DEVICES=${device} python test.py --dataset mvtec \
+#         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/mvtecdataset --save_path ./results/${base_dir}/mvtec \
+#         --checkpoint_path ${save_dir}epoch_15.pth \
+#         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+#         --print_top_k 10
+#     wait
+#     done
+# done
+# python ${NOTIFY_SCRIPT} --mode test --dataset mvtec --log_path ./results/${base_dir}/mvtec/log.txt --exp_name ${base_dir}
 
-LOG=${save_dir}"res.log"
-echo ${LOG}
-depth=(9)
-n_ctx=(12)
-t_n_ctx=(4)
-for i in "${!depth[@]}";do
-    for j in "${!n_ctx[@]}";do
-    ## train on the VisA dataset
-        base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
-        save_dir=./checkpoints/${base_dir}/
-        CUDA_VISIBLE_DEVICES=${device} python test.py --dataset visa \
-        --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/visa --save_path ./results/${base_dir}/visa \
-        --checkpoint_path ${save_dir}epoch_15.pth \
-        --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
-        --print_top_k 10
-    wait
-    done
-done
-python ${NOTIFY_SCRIPT} --mode test --dataset visa --log_path ./results/${base_dir}/visa/log.txt --exp_name ${base_dir}
+# LOG=${save_dir}"res.log"
+# echo ${LOG}
+# depth=(9)
+# n_ctx=(12)
+# t_n_ctx=(4)
+# for i in "${!depth[@]}";do
+#     for j in "${!n_ctx[@]}";do
+#     ## train on the VisA dataset
+#         base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
+#         save_dir=./checkpoints/${base_dir}/
+#         CUDA_VISIBLE_DEVICES=${device} python test.py --dataset visa \
+#         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/visa --save_path ./results/${base_dir}/visa \
+#         --checkpoint_path ${save_dir}epoch_15.pth \
+#         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+#         --print_top_k 10
+#     wait
+#     done
+# done
+# python ${NOTIFY_SCRIPT} --mode test --dataset visa --log_path ./results/${base_dir}/visa/log.txt --exp_name ${base_dir}
 
-LOG=${save_dir}"res.log"
-echo ${LOG}
-depth=(9)
-n_ctx=(12)
-t_n_ctx=(4)
-for i in "${!depth[@]}";do
-    for j in "${!n_ctx[@]}";do
-    ## train on the VisA dataset
-        base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
-        save_dir=./checkpoints/${base_dir}/
-        CUDA_VISIBLE_DEVICES=${device} python test.py --dataset mpdd \
-        --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/MPDD --save_path ./results/${base_dir}/mpdd \
-        --checkpoint_path ${save_dir}epoch_15.pth \
-        --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
-        --print_top_k 10
-    wait
-    done
-done
-python ${NOTIFY_SCRIPT} --mode test --dataset mpdd --log_path ./results/${base_dir}/mpdd/log.txt --exp_name ${base_dir}
+# LOG=${save_dir}"res.log"
+# echo ${LOG}
+# depth=(9)
+# n_ctx=(12)
+# t_n_ctx=(4)
+# for i in "${!depth[@]}";do
+#     for j in "${!n_ctx[@]}";do
+#     ## train on the VisA dataset
+#         base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
+#         save_dir=./checkpoints/${base_dir}/
+#         CUDA_VISIBLE_DEVICES=${device} python test.py --dataset mpdd \
+#         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/MPDD --save_path ./results/${base_dir}/mpdd \
+#         --checkpoint_path ${save_dir}epoch_15.pth \
+#         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+#         --print_top_k 10
+#     wait
+#     done
+# done
+# python ${NOTIFY_SCRIPT} --mode test --dataset mpdd --log_path ./results/${base_dir}/mpdd/log.txt --exp_name ${base_dir}
 
-LOG=${save_dir}"res.log"
-echo ${LOG}
-depth=(9)
-n_ctx=(12)
-t_n_ctx=(4)
-for i in "${!depth[@]}";do
-    for j in "${!n_ctx[@]}";do
-    ## train on the VisA dataset
-        base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
-        save_dir=./checkpoints/${base_dir}/
-        CUDA_VISIBLE_DEVICES=${device} python test.py --dataset DAGM_KaggleUpload \
-        --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/DAGM/DAGM_KaggleUpload --save_path ./results/${base_dir}/dagm \
-        --checkpoint_path ${save_dir}epoch_15.pth \
-        --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
-        --print_top_k 10
-    wait
-    done
-done
-python ${NOTIFY_SCRIPT} --mode test --dataset DAGM_KaggleUpload --log_path ./results/${base_dir}/dagm/log.txt --exp_name ${base_dir}
+# LOG=${save_dir}"res.log"
+# echo ${LOG}
+# depth=(9)
+# n_ctx=(12)
+# t_n_ctx=(4)
+# for i in "${!depth[@]}";do
+#     for j in "${!n_ctx[@]}";do
+#     ## train on the VisA dataset
+#         base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
+#         save_dir=./checkpoints/${base_dir}/
+#         CUDA_VISIBLE_DEVICES=${device} python test.py --dataset DAGM_KaggleUpload \
+#         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/DAGM/DAGM_KaggleUpload --save_path ./results/${base_dir}/dagm \
+#         --checkpoint_path ${save_dir}epoch_15.pth \
+#         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+#         --print_top_k 10
+#     wait
+#     done
+# done
+# python ${NOTIFY_SCRIPT} --mode test --dataset DAGM_KaggleUpload --log_path ./results/${base_dir}/dagm/log.txt --exp_name ${base_dir}
 
-LOG=${save_dir}"res.log"
-echo ${LOG}
-depth=(9)
-n_ctx=(12)
-t_n_ctx=(4)
-for i in "${!depth[@]}";do
-    for j in "${!n_ctx[@]}";do
-    ## train on the VisA dataset
-        base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
-        save_dir=./checkpoints/${base_dir}/
-        CUDA_VISIBLE_DEVICES=${device} python test.py --dataset SDD \
-        --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/SDD --save_path ./results/${base_dir}/sdd \
-        --checkpoint_path ${save_dir}epoch_15.pth \
-        --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
-        --print_top_k 10
+# LOG=${save_dir}"res.log"
+# echo ${LOG}
+# depth=(9)
+# n_ctx=(12)
+# t_n_ctx=(4)
+# for i in "${!depth[@]}";do
+#     for j in "${!n_ctx[@]}";do
+#     ## train on the VisA dataset
+#         base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
+#         save_dir=./checkpoints/${base_dir}/
+#         CUDA_VISIBLE_DEVICES=${device} python test.py --dataset SDD \
+#         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/SDD --save_path ./results/${base_dir}/sdd \
+#         --checkpoint_path ${save_dir}epoch_15.pth \
+#         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+#         --print_top_k 10
 
-    wait
-    done
-done
-python ${NOTIFY_SCRIPT} --mode test --dataset SDD --log_path ./results/${base_dir}/sdd/log.txt --exp_name ${base_dir}
+#     wait
+#     done
+# done
+# python ${NOTIFY_SCRIPT} --mode test --dataset SDD --log_path ./results/${base_dir}/sdd/log.txt --exp_name ${base_dir}
 
 
-LOG=${save_dir}"res.log"
-echo ${LOG}
-depth=(9)
-n_ctx=(12)
-t_n_ctx=(4)
-for i in "${!depth[@]}";do
-    for j in "${!n_ctx[@]}";do
-    ## train on the VisA dataset
-        base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
-        save_dir=./checkpoints/${base_dir}/
-        CUDA_VISIBLE_DEVICES=${device} python test.py --dataset DTD \
-        --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/DTD-Synthetic --save_path ./results/${base_dir}/dtd \
-        --checkpoint_path ${save_dir}epoch_15.pth \
-        --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
-        --print_top_k 10
-    wait
-    done
-done
-python ${NOTIFY_SCRIPT} --mode test --dataset dtd-synthetic --log_path ./results/${base_dir}/dtd/log.txt --exp_name ${base_dir}
+# LOG=${save_dir}"res.log"
+# echo ${LOG}
+# depth=(9)
+# n_ctx=(12)
+# t_n_ctx=(4)
+# for i in "${!depth[@]}";do
+#     for j in "${!n_ctx[@]}";do
+#     ## train on the VisA dataset
+#         base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
+#         save_dir=./checkpoints/${base_dir}/
+#         CUDA_VISIBLE_DEVICES=${device} python test.py --dataset DTD \
+#         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/DTD-Synthetic --save_path ./results/${base_dir}/dtd \
+#         --checkpoint_path ${save_dir}epoch_15.pth \
+#         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+#         --print_top_k 10
+#     wait
+#     done
+# done
+# python ${NOTIFY_SCRIPT} --mode test --dataset dtd-synthetic --log_path ./results/${base_dir}/dtd/log.txt --exp_name ${base_dir}
 
-LOG=${save_dir}"res.log"
-echo ${LOG}
-depth=(9)
-n_ctx=(12)
-t_n_ctx=(4)
-for i in "${!depth[@]}";do
-    for j in "${!n_ctx[@]}";do
-    ## train on the VisA dataset
-        base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
-        save_dir=./checkpoints/${base_dir}/
-        CUDA_VISIBLE_DEVICES=${device} python test.py --dataset btad \
-        --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/btad/BTech_Dataset_transformed --save_path ./results/${base_dir}/btad \
-        --checkpoint_path ${save_dir}epoch_15.pth \
-        --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
-        --print_top_k 10
-    wait
-    done
-done
+# LOG=${save_dir}"res.log"
+# echo ${LOG}
+# depth=(9)
+# n_ctx=(12)
+# t_n_ctx=(4)
+# for i in "${!depth[@]}";do
+#     for j in "${!n_ctx[@]}";do
+#     ## train on the VisA dataset
+#         base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
+#         save_dir=./checkpoints/${base_dir}/
+#         CUDA_VISIBLE_DEVICES=${device} python test.py --dataset btad \
+#         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/btad/BTech_Dataset_transformed --save_path ./results/${base_dir}/btad \
+#         --checkpoint_path ${save_dir}epoch_15.pth \
+#         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+#         --print_top_k 10
+#     wait
+#     done
+# done
 
 LOG=${save_dir}"res.log"
 echo ${LOG}
@@ -158,6 +158,7 @@ for i in "${!depth[@]}";do
         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/br35/ --save_path ./results/${base_dir}/br35 \
         --checkpoint_path ${save_dir}epoch_15.pth \
         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+        --metrics image-level \
         --print_top_k 10
     wait
     done
@@ -177,6 +178,7 @@ for i in "${!depth[@]}";do
         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/BrainMRI/ --save_path ./results/${base_dir}/BrainMRI \
         --checkpoint_path ${save_dir}epoch_15.pth \
         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+        --metrics image-level \
         --print_top_k 10
     wait
     done
@@ -196,6 +198,7 @@ for i in "${!depth[@]}";do
         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/CVC/CVC-ClinicDB/ --save_path ./results/${base_dir}/ClinicDB \
         --checkpoint_path ${save_dir}epoch_15.pth \
         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+        --metrics pixel-level \
         --print_top_k 10
     wait
     done
@@ -215,6 +218,7 @@ for i in "${!depth[@]}";do
         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/CVC/CVC-ColonDB/ --save_path ./results/${base_dir}/ColonDB \
         --checkpoint_path ${save_dir}epoch_15.pth \
         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+        --metrics pixel-level \
         --print_top_k 10
     wait
     done
@@ -230,10 +234,11 @@ for i in "${!depth[@]}";do
     ## train on the VisA dataset
         base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
         save_dir=./checkpoints/${base_dir}/
-        CUDA_VISIBLE_DEVICES=${device} python test.py --dataset chest \
+        CUDA_VISIBLE_DEVICES=${device} python test.py --dataset Chest \
         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/Covid/ --save_path ./results/${base_dir}/Covid \
         --checkpoint_path ${save_dir}epoch_15.pth \
         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+        --metrics image-level \
         --print_top_k 10
     wait
     done
@@ -253,6 +258,7 @@ for i in "${!depth[@]}";do
         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/EndoTect_2020_Segmentation_Test_Dataset/ --save_path ./results/${base_dir}/EndoTect \
         --checkpoint_path ${save_dir}epoch_15.pth \
         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+        --metrics pixel-level \
         --print_top_k 10
     wait
     done
@@ -272,6 +278,7 @@ for i in "${!depth[@]}";do
         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/HeadCT_anomaly_detection/ --save_path ./results/${base_dir}/HeadCT \
         --checkpoint_path ${save_dir}epoch_15.pth \
         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+        --metrics image-level \
         --print_top_k 10
     wait
     done
@@ -287,10 +294,11 @@ for i in "${!depth[@]}";do
     ## train on the VisA dataset
         base_dir=${depth[i]}_${n_ctx[j]}_${t_n_ctx[0]}_multiscale_visa
         save_dir=./checkpoints/${base_dir}/
-        CUDA_VISIBLE_DEVICES=${device} python test.py --dataset skin \
+        CUDA_VISIBLE_DEVICES=${device} python test.py --dataset ISBI \
         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/ISIC/ --save_path ./results/${base_dir}/ISIC \
         --checkpoint_path ${save_dir}epoch_15.pth \
         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+        --metrics pixel-level \
         --print_top_k 10
     wait
     done
@@ -310,6 +318,7 @@ for i in "${!depth[@]}";do
         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/CVC/Kvasir/ --save_path ./results/${base_dir}/Kvasir \
         --checkpoint_path ${save_dir}epoch_15.pth \
         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+        --metrics pixel-level \
         --print_top_k 10
     wait
     done
@@ -329,6 +338,7 @@ for i in "${!depth[@]}";do
         --data_path /home/ai3/NguyenND/ZeroShotAD-CLIP/data/TN3K/ --save_path ./results/${base_dir}/TN3K \
         --checkpoint_path ${save_dir}epoch_15.pth \
         --features_list 24 --image_size 518 --depth ${depth[i]} --n_ctx ${n_ctx[j]} --t_n_ctx ${t_n_ctx[0]} \
+        --metrics pixel-level \
         --print_top_k 10
     wait
     done
